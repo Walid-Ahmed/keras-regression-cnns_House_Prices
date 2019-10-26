3c17e1f9b675caf3b2329d08b958f281b70e8aca
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-10-25 22:14:05 -0400

    adding images

--- cnn_regression.py
+++ cnn_regression.py
@@ -1,11 +1,10 @@
 # USAGE
-# python cnn_regression.py --dataset Houses-dataset/Houses\ Dataset/
+# python cnn_regression.py 
 
 # import the necessary packages
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-from pyimagesearch import datasets
-from pyimagesearch import models
+#from pyimagesearch import datasets
 import numpy as np
 import argparse
 import locale
@@ -17,22 +16,55 @@ import numpy as np
 from pathlib import Path
 import itertools
 
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+
+#from pyimagesearch import datasets
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", type=str, required=True,
-	help="path to input dataset of house images")
-args = vars(ap.parse_args())
 
+EPOCHS_NUM=200
 # construct the path to the input .txt file that contains information
 # on each house in the dataset and then load the dataset
+
 print("[INFO] loading house attributes...")
-inputPath = os.path.sep.join([args["dataset"], "HousesInfo.txt"])
-df= datasets.load_house_attributes(inputPath)
+inputPath =  "HousesInfo.txt"
+cols = ["bedrooms", "bathrooms", "area", "zipcode", "price"]
+df = pd.read_csv(inputPath, sep=" ", header=None, names=cols)
+print(df.head())
 
 
-recordsIndicies=df.index.values
+#remove zipcounts that have kess than 25 houses
+#Pandas Index.value_counts() function returns object containing counts of unique values. The resulting object will be in descending order so that the first element is the most frequently-occurring element. Excludes NA values by default.
+zipcodeSeries=df["zipcode"].value_counts()  #<class 'pandas.core.series.Series'>
+zipcodes = zipcodeSeries.keys().tolist()   #zipcodes as list
+counts = zipcodeSeries.tolist()    #count of zipcodes as list  
+for (zipcode, count) in zip(zipcodes, counts):
+		# the zip code counts for our housing dataset is *extremely*
+		# unbalanced (some only having 1 or 2 houses per zip code)
+		# so let's sanitize our data by removing any houses with less
+		# than 25 houses per zip code
+		if count < 25:
+			booleanVal=(df["zipcode"] == zipcode)  # this will be true at all zipcodes that should be deleted
+			#print(type(booleanVal))   #<class 'pandas.core.series.Series'>
+			idxs = df[booleanVal].index  #this will return indices of these true values
+			df.drop(idxs, inplace=True)
+print("[INFO]removed zipcodes which less than 25 houses")            
+
+
 
 
 
@@ -59,7 +91,7 @@ for imagePath in imagesPaths:
 
 #read image and concotnat each 4 images into one image
 trainingImages=[]
-for recordIndex in recordsIndicies:
+for recordIndex in df.index:
 	dirOfImages=os.path.join("HouseImages",str(recordIndex))
 	houseImages=[]
 	files=os.listdir(dirOfImages)
@@ -87,8 +119,7 @@ for recordIndex in recordsIndicies:
 
 images = np.array(trainingImages, dtype="float") / 255.0
 
-print("Shapee of images data set {}".format(images.shape))
-print("Shapee of price  vector {}".format(df.shape))
+
 
 
 
@@ -104,47 +135,123 @@ images = images / 255.0
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
 split = train_test_split(df, images, test_size=0.25, random_state=42)
-(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+(trainY, testY, trainX, testX) = split
 
 # find the largest house price in the training set and use it to
 # scale our house prices to the range [0, 1] (will lead to better
 # training and convergence)
-maxPrice = trainAttrX["price"].max()
-trainY = trainAttrX["price"] / maxPrice
-testY = testAttrX["price"] / maxPrice
+maxPrice = trainY["price"].max()
+trainY = trainY["price"] / maxPrice
+testY = testY["price"] / maxPrice
+
+
+print("Shapee of training  data set {}".format(trainX.shape))
+print("Shapee of price  vector {}".format(testX.shape))
 
 # create our Convolutional Neural Network and then compile the model
 # using mean absolute percentage error as our loss, implying that we
 # seek to minimize the absolute percentage difference between our
-# price *predictions* and the *actual prices*
-model = models.create_cnn(64, 64, 3, regress=True)
+# price *predictions* and the *actual prices
+
+
+inputShape = (64, 64, 3)
+chanDim = -1
+# define the model input
+inputs = Input(shape=inputShape)
+# CONV => RELU => BN => POOL
+x = Conv2D(16, (3, 3), padding="same")(inputs)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+# CONV => RELU => BN => POOL
+x = Conv2D(32, (3, 3), padding="same")(x)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+# CONV => RELU => BN => POOL
+x = Conv2D(64, (3, 3), padding="same")(x)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# flatten the volume, then FC => RELU => BN => DROPOUT
+x = Flatten()(x)
+x = Dense(16)(x)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = Dropout(0.5)(x)
+
+# apply another FC layer, this one to match the number of nodes
+# coming out of the MLP
+x = Dense(4)(x)
+x = Activation("relu")(x)
+x = Dense(1, activation="linear")(x)
+
+# construct the CNN
+model = Model(inputs, x)
+
+
+
+
+#model = models.create_cnn(64, 64, 3, regress=True)
+model.summary()
+
 opt = Adam(lr=1e-3, decay=1e-3 / 200)
 model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
 
 # train the model
 print("[INFO] training model...")
-model.fit(trainImagesX, trainY, validation_data=(testImagesX, testY),
-	epochs=200, batch_size=8)
+history=model.fit(trainX, trainY, validation_data=(testX, testY),epochs=EPOCHS_NUM, batch_size=8)
 
 # make predictions on the testing data
 print("[INFO] predicting house prices...")
-preds = model.predict(testImagesX)
+preds = model.predict(testX)
 
-# compute the difference between the *predicted* house prices and the
-# *actual* house prices, then compute the percentage difference and
-# the absolute percentage difference
-diff = preds.flatten() - testY
-percentDiff = (diff / testY) * 100
-absPercentDiff = np.abs(percentDiff)
 
-# compute the mean and standard deviation of the absolute percentage
-# difference
-mean = np.mean(absPercentDiff)
-std = np.std(absPercentDiff)
 
-# finally, show some statistics on our model
-locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
-print("[INFO] avg. house price: {}, std house price: {}".format(
-	locale.currency(df["price"].mean(), grouping=True),
-	locale.currency(df["price"].std(), grouping=True)))
-print("[INFO] mean: {:.2f}%, std: {:.2f}%".format(mean, std))
+model.save("housePrice.keras2")
+print("[INFO] model saved to housePrice.keras2")
+
+# make predictions on the testing data
+print("[INFO] predicting house prices...")
+preds = model.predict(testX)
+
+validationLoss=(history.history['val_loss'])
+trainingLoss=history.history['loss']
+
+
+
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+epochs   = range(len(validationLoss)) # Get number of epochs
+ #------------------------------------------------
+plt.plot  ( epochs,     trainingLoss ,label="Training Loss")
+plt.plot  ( epochs, validationLoss, label="Validation Loss" )
+plt.title ('Training and validation loss')
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
+fileToSaveAccuracyCurve="plot_acc.png"
+plt.savefig("plot_acc.png")
+print("[INFO] Loss curve saved to {}".format("plot_acc.png"))
+plt.legend(loc="upper right")
+plt.show()
+
+
+
+#readjust house prices
+testY=testY*maxPrice
+preds=preds*maxPrice
+#plot curves (Actual vs Predicted)
+plt.plot  ( testY ,label="Actual price")
+plt.plot  ( preds, label="Predicted price" )
+plt.title ('House prices')
+plt.xlabel("Point #")
+plt.ylabel("Price")
+plt.legend(loc="upper right")
+plt.show()
+plt.savefig("HousePrices.png")
+print("[INFO] predicted vs actual price saved to HousePrices.png")
+
+
+
